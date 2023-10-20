8848e6862cfec56b7b1b7417b62b9cdb57380a98
Saikat Chatterjee <csaikat66@gmail.com>
2023-10-20 15:51:08 +0530

    add dark theam

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,24 @@
+let option_container = document.getElementById('nav-profile-img');
 function optionShow(){
-	let option_container = document.getElementById('nav-profile-img');
 	option_container.classList.toggle('show-option');
+}
+
+//make setting menu option dark
+function darkMode(){
+	document.body.classList.toggle('dark-mode');
+	if(localStorage.getItem("theam")=="light"){
+		localStorage.setItem("theam","dark");
+		
+	}else{
+		localStorage.setItem("theam","light");
+	}
+}
+
+if(localStorage.getItem("theam")=="light"){
+	document.body.classList.remove('dark-mode');
+}else if(localStorage.getItem("theam")=="dark"){
+	document.body.classList.add('dark-mode');
+	document.getElementsByClassName("check_box")[0].checked = true;
+}else{
+	localStorage.setItem("theam","light");
 }
